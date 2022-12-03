f3c820de53c978d941ca785d3099af1aaadd3ab7
ProfAnol <117153794+ProfAnol@users.noreply.github.com>
2022-12-03 11:21:11 +0200

    Add files via upload

---
+++ gray.py
@@ -0,0 +1,12 @@
+﻿import cv2
+import numpy as np
+#读取图片
+image = cv2.imread("D:\\MASKRCNN\\images\\test2.jpg")     
+cv2.imshow('original',image)
+
+#灰度处理
+im_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+cv2.imshow('gray',im_gray)
+
+cv2.waitKey()
+cv2.destroyAllWindows()
